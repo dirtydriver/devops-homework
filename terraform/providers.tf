6a15f633d3d60ab87a3f683e3b6b8392f9c2d0ce
Akos Petruska <petruskaakos@gmail.com>
2026-09-13 22:20:46 +0200

    feat: implement terraform part and add tfvars example

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,27 @@
 terraform {
-    required_version = ">= 1.5"
+  required_version = ">= 1.5"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+  }
 }
 
+
+
+
 provider "kubernetes" {
-    config_path = "~/.kube/config"
+  config_path = pathexpand(var.kubeconfig_path)
 }
 
 provider "helm" {
-    kubernetes {
-        config_path = "~/.kube/config"
-    }
+  kubernetes = {
+    config_path = pathexpand(var.kubeconfig_path)
+  }
 }
